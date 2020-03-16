11813a549064f64bc4e2e7174df8d743e2ea47e4
Andarus-1994 <andrei_king1@yahoo.com>
2020-03-16 15:08:16 +0200

    Updates

---
+++ precache-manifest.f5713e1ed87c872971c755b31d7a9a4c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8f4deae38f54cc9e80ac8076408236",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5062e023cf0f8b165178",
+    "url": "/Portfolio/static/css/2.caed52fd.chunk.css"
+  },
+  {
+    "revision": "ed4ff3d363f64775ea1a",
+    "url": "/Portfolio/static/css/main.f5e4afc2.chunk.css"
+  },
+  {
+    "revision": "5062e023cf0f8b165178",
+    "url": "/Portfolio/static/js/2.b38c8c97.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Portfolio/static/js/2.b38c8c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4ff3d363f64775ea1a",
+    "url": "/Portfolio/static/js/main.3a2eb2a8.chunk.js"
+  },
+  {
+    "revision": "f05f7305b86207cd2b64",
+    "url": "/Portfolio/static/js/runtime-main.d24a7eec.js"
+  },
+  {
+    "revision": "e61b081f008be280e7a9be59b02306c1",
+    "url": "/Portfolio/static/media/bms2.e61b081f.jpg"
+  },
+  {
+    "revision": "19b93e849223b20236b318a01444e00a",
+    "url": "/Portfolio/static/media/dog.19b93e84.png"
+  },
+  {
+    "revision": "5a457da7369f5e530b32482d2f9f4622",
+    "url": "/Portfolio/static/media/img_Carousel1.5a457da7.png"
+  },
+  {
+    "revision": "2fe8515ea1ac9a5133eab376a4d48459",
+    "url": "/Portfolio/static/media/img_Carousel2.2fe8515e.png"
+  },
+  {
+    "revision": "e47c5a968bf1b2af8f1ecd7b46c1bac9",
+    "url": "/Portfolio/static/media/natureProject.e47c5a96.png"
+  },
+  {
+    "revision": "a589e8f72c0df6e626204a584e16286e",
+    "url": "/Portfolio/static/media/project2.a589e8f7.jpg"
+  },
+  {
+    "revision": "62c618d11357e0c1335e48970f9df88f",
+    "url": "/Portfolio/static/media/project3.62c618d1.png"
+  }
+]);
